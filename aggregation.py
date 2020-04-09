02535da855d8ce8ea91a3c84e10e8609c6fe08fe
Scott Lowry <slowry010@gmail.com>
2020-04-09 07:32:59 -0700

    added endpoints for summary and countries. returning json

--- aggregation.py
+++ aggregation.py
@@ -1,21 +1,28 @@
 from flask import Flask, jsonify, request, make_response
 import requests
 import json
+from urllib.request import urlopen
 
 
 app = Flask(__name__)
 
 
-@app.route("/api/covid19_data", methods=['GET'])
-def get_ingest_covid19_data():
+@app.route("/api/covid19_summary_data", methods=['GET'])
+def get_ingest_covid19_summary_data():
     print('in here 1')
-    response = requests.get("https://api.covid19api.com/all")#"http://localhost:8000/")
-    #json_data = json.loads(response.text)
+    response = requests.get("http://localhost:8000/summary")
+    json_data = json.loads(response.text)
     print('in here 2')
-    # print(r)
-    # print('in here 3')
-    # return jsonify(r.text)
-    return response.text
+    return json_data
+
+
+@app.route("/api/covid19_countries_data", methods=['GET'])
+def get_ingest_covid19_countries_data():
+    print('in here 1')
+    response = requests.get("http://localhost:8000/countries")
+    json_data = json.loads(response.text)
+    print('in here 2')
+    return json_data
 
 
 if __name__ == '__main__':
